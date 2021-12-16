5abd48c337a95924cf330802a59ca1fbb9653fa9
vietvd13 <vuducviet0131@gmail.com>
2021-12-16 21:37:42 +0700

    [FE][Update UI Login Page]

--- resources/js/pages/Login/index.vue
+++ resources/js/pages/Login/index.vue
@@ -4,42 +4,48 @@
             <div class="login-form col-xl-6 col-lg-6 mx-auto">
                 <div class="login-form-content">
                     <b-row>
-                    <b-col>
-                        <!-- Title -->
-                        <div class="login-form-content__header">
-                        <h1>{{ $t('LOGIN.TITLE') }}</h1>
-                        </div>
+                        <b-col>
+                            <!-- Title -->
+                            <div class="login-form-content__header">
+                                <h1>{{ $t('LOGIN.TITLE') }}</h1>
+                            </div>
 
-                        <!-- Account -->
-                        <div class="login-form-content__body">
-                        <div class="item-input">
-                            <b-form-input
-                            v-model="User.account"
-                            type="text"
-                            spellcheck="false"
-                            :placeholder="$t('LOGIN.PLACEHOLDER_ACCOUNT')"
-                            />
-                        </div>
+                            <!-- Account -->
+                            <div class="login-form-content__body">
+                                <div class="item-input">
+                                    <b-form-input
+                                        v-model="User.account"
+                                        type="text"
+                                        spellcheck="false"
+                                        :placeholder="$t('LOGIN.PLACEHOLDER_ACCOUNT')"
+                                    />
+                                </div>
 
-                        <div class="item-input">
-                            <b-form-input
-                            v-model="User.password"
-                            type="password"
-                            spellcheck="false"
-                            :placeholder="$t('LOGIN.PLACEHOLDER_PASSWORD')"
-                            />
-                        </div>
-                        </div>
+                                <div class="item-input">
+                                    <b-input-group>
+                                        <b-form-input
+                                            v-model="User.password"
+                                            :type="showPassword ? 'text' : 'password'"
+                                            spellcheck="false"
+                                            autocomplete="new-password"
+                                            :placeholder="$t('LOGIN.PLACEHOLDER_PASSWORD')"
+                                        />
+                                        <b-input-group-append is-text>
+                                            <i @click="showPassword = !showPassword" :class="showPassword ? 'fas fa-eye-slash' : 'fas fa-eye'"></i>
+                                        </b-input-group-append>
+                                    </b-input-group>
+                                </div>
+                            </div>
 
-                        <div class="login-form-content__footer">
-                        <b-row>
-                            <b-col>
-                            <b-button>{{ $t('LOGIN.BUTTON_LOGIN') }}</b-button>
-                            </b-col>
-                        </b-row>
-                        </div>
+                            <div class="login-form-content__footer">
+                                <b-row>
+                                    <b-col>
+                                        <b-button>{{ $t('LOGIN.BUTTON_LOGIN') }}</b-button>
+                                    </b-col>
+                                </b-row>
+                            </div>
 
-                    </b-col>
+                        </b-col>
                     </b-row>
                 </div>
             </div>
@@ -56,6 +62,7 @@ export default {
                 account: '',
                 password: '',
             },
+            showPassword: false,
         };
     },
 };

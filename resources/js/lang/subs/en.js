efdb62a9e8671e7432f07d78f002a1224f9bfd98
vietvd13 <vuducviet0131@gmail.com>
2022-01-06 11:09:53 +0700

    [FE] Update page 404

--- resources/js/lang/subs/en.js
+++ resources/js/lang/subs/en.js
@@ -5,12 +5,17 @@ export default {
 		DANGER: 'Danger'
 	},
 	ROUTER: {
-		LOGIN: 'Login'
+		LOGIN: 'Login',
+		PAGE_NOT_FOUND: 'Page Not Found'
 	},
 	LOGIN: {
 		TITLE: 'Login',
 		PLACEHOLDER_ACCOUNT: 'Enter your account',
 		PLACEHOLDER_PASSWORD: 'Enter your password',
 		BUTTON_LOGIN: 'Login'
+	},
+	PAGE_NOT_FOUND: {
+		DETAIL: "We can't seem to find the page you're looking for",
+		GO_TO_HOME: 'Go To Home Page'
 	}
 };
--- resources/js/router/modules/error.js
+++ resources/js/router/modules/error.js
@@ -2,7 +2,7 @@ const errorRoute = {
 	path: '/page-not-found',
 	name: 'PageNotFound',
 	meta: {
-		title: 'ROUTER.LOGIN',
+		title: 'ROUTER.PAGE_NOT_FOUND',
 		icon: ''
 	},
 	hidden: true,

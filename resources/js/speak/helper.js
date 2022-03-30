fd0089627003d31644e5ebf64360d5f164d25d50
Vu Duc Viet <vuducviet0131@gmail.com>
2022-03-30 17:27:43 +0700

    [FE][Record audio]

--- resources/js/speak/helper.js
+++ resources/js/speak/helper.js
@@ -1,7 +1,29 @@
 import Cookies from 'js-cookie';
 
+const STATUS_PERMISSION = 'STATUS_PERMISSION';
 const STATUS_RECORD = 'STATUS_RECORD';
+const STATUS_PROCESS = 'STATUS_PROCESS';
 
+// STATUS_PERMISSION
+export function getStatusPermission() {
+	const GET_STATUS_PERMISSION = Cookies.get(STATUS_PERMISSION);
+
+	if (GET_STATUS_PERMISSION) {
+		return GET_STATUS_PERMISSION;
+	}
+
+	return '0';
+}
+
+export function setHasPermission() {
+	Cookies.set(STATUS_PERMISSION, '1');
+}
+
+export function setNotHasPermission() {
+	Cookies.set(STATUS_PERMISSION, '0');
+}
+
+// STATUS_RECORD
 export function getIsRecord() {
 	const GET_STATUS_RECORD = Cookies.get(STATUS_RECORD);
 
@@ -18,4 +40,23 @@ export function setStartRecord() {
 
 export function setEndRecord() {
 	Cookies.set(STATUS_RECORD, '0');
+}
+
+// STATUS_PROCESS
+export function getStatusProccess() {
+	const GET_STATUS_PROCESS = Cookies.get(STATUS_PROCESS);
+
+	if (GET_STATUS_PROCESS) {
+		return GET_STATUS_PROCESS;
+	}
+
+	return '0';
+}
+
+export function setStartProcess() {
+	Cookies.set(STATUS_PROCESS, '1');
+}
+
+export function setEndProcess() {
+	Cookies.set(STATUS_PROCESS, '0');
 }
--- resources/js/speak/record.js
+++ resources/js/speak/record.js
@@ -1,11 +1,49 @@
-import { getIsRecord, setStartRecord, setEndRecord } from './helper';
+import AudioRecorder from 'audio-recorder-polyfill';
+import mpegEncoder from 'audio-recorder-polyfill/mpeg-encoder';
+import {
+	getIsRecord,
+	setStartRecord,
+	setEndRecord,
+	getStatusPermission,
+	setHasPermission,
+	setNotHasPermission,
+	getStatusProccess,
+	setStartProcess,
+	setEndProcess
+} from './helper';
+
+AudioRecorder.encoder = mpegEncoder;
+AudioRecorder.prototype.mimeType = 'audio/mp3';
+window.MediaRecorder = AudioRecorder;
+
+let recorder;
+
+navigator.mediaDevices
+	.getUserMedia({ audio: true })
+	.then(stream => {
+		recorder = new MediaRecorder(stream);
+		clearRecord();
+		setHasPermission();
+
+		recorder.addEventListener('dataavailable', e => {
+			setStartProcess();
+			console.log(e.data);
+			setEndProcess();
+		});
+	})
+	.catch(error => {
+		setNotHasPermission();
+		console.log(error);
+	});
 
 export function handleRecord(key) {
 	window.onkeydown = event => {
 		if ((event.code === key.code || event.keyCode === key.keyCode) && event.ctrlKey) {
 			event.preventDefault();
 
-			startRecord();
+			if (getStatusPermission() === '1') {
+				handleStartRecord();
+			}
 		}
 	};
 
@@ -13,27 +51,45 @@ export function handleRecord(key) {
 		if ((event.code === key.code || event.keyCode === key.keyCode) && event.ctrlKey) {
 			event.preventDefault();
 
-			endRecord();
+			if (getStatusPermission() === '1') {
+				handleEndRecord();
+			}
 		}
 	};
 }
 
-function startRecord() {
-	if (getIsRecord() === '0') {
+function handleStartRecord() {
+	if (getIsRecord() === '0' && getStatusProccess() === '0') {
 		setStartRecord();
+
+		startRecord();
 
 		console.log('Start Record');
 
 		setTimeout(() => {
 			if (getIsRecord() === '1') {
-				endRecord();
+				handleEndRecord();
 			}
 		}, 10000);
 	}
 }
 
-function endRecord() {
+function handleEndRecord() {
 	setEndRecord();
+	endRecord();
+	clearRecord();
 
 	console.log('End Record');
+}
+
+function startRecord() {
+	recorder.start();
+}
+
+function endRecord() {
+	recorder.stop();
+}
+
+function clearRecord() {
+	recorder.stream.getTracks().forEach(i => i.stop());
 }
